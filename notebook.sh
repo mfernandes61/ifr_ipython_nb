c17c7716c16e5cf2f7304906d71dc972124f6f05
Mark Fernandes <mfernandes61@gmail.com>
2016-12-22 11:19:10 +0000

    Update notebook.sh

--- notebook.sh
+++ notebook.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 IFS=$'\n\t' 
 
 # Create a self signed certificate for the user if one doesn't exist
-if [ ! -f $PEM_FILE ]; then
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PEM_FILE -out $PEM_FILE \
-    -subj "/C=XX/ST=XX/L=XX/O=dockergenerated/CN=dockergenerated"
-fi
+# if [ ! -f $PEM_FILE ]; then
+#  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PEM_FILE -out $PEM_FILE \
+#    -subj "/C=XX/ST=XX/L=XX/O=dockergenerated/CN=dockergenerated"
+# fi
 
 #HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
 unset PASSWORD
